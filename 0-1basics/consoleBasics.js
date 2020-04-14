d65c1d3be59c2338726ba0a469338284b333e137
Chetan Gupta <chetan.gupta@mtxb2b.com>
2020-04-14 07:48:15 +0530

    First commit of the day

---
+++ 0-1basics/consoleBasics.js
@@ -0,0 +1,9 @@
+console.log('Hello world');
+console.log({
+    name: 'Shahrukh Khan',
+    age: 54,
+    occupation: 'actor'
+});
+window.alert('This is the first program');
+window.prompt('Enter value');
+window.confirm('Are you sure you want to do 100days of Coding Challenge');
