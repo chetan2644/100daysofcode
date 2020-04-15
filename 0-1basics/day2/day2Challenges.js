037d18747a9fb0e0263fecdaeae460063dae8038
Chetan Gupta <chetan.gupta@mtxb2b.com>
2020-04-15 09:38:17 +0530

    Day2
    It covers console, variable, datatypes, comments, built in constants.

---
+++ 0-1basics/day2/day2Challenges.js
@@ -0,0 +1,9 @@
+//defining a var:
+var data = 'this is a string var';
+var favoriteFruits = ['apples', 'bananas', 'strawberries'];
+var carsInParking = ['Alto', 'Swift', 'Verna', 'XUV'];
+var person = {
+    name: 'Chetan',
+    age: 23,
+    gender: 'Male'
+}
