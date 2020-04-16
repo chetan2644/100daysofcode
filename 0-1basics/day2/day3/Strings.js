9fbe18a0d3d42f3e52a2076920d584d631dde2fa
Chetan Gupta <chetan.gupta@mtxb2b.com>
2020-04-16 08:45:41 +0530

    Day 3
    String
    String comparison
    Date
    Date comparison
    Comparison operator.

---
+++ 0-1basics/day2/day3/Strings.js
@@ -0,0 +1,24 @@
+//simple string
+var stringExample = 'Heloo';
+// String constructor.
+var stringConst = String(32);
+console.log(typeof stringConst);
+
+var constring = (32).toString();
+var concatenating = 'Hello' + ' World!';
+console.log(concatenating);
+
+var dateExample = new Date(); // this will give the current date and time .
+var day = getDate(dateExample); //this will give the date i.e 12 or 13 just the number.
+//common methods of js date.
+// getDate();
+// setDate();
+// getFullYear();
+// convert date to dateString or dateString to date();
+// getDay(); to get the current day .
+
+var a = 4;
+var b = 8;
+console.log( a > 4);
+// === vs == 
+// type of operator.
